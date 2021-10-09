31b264d5fc5d9d23b13c252930edbd21db1037cb
EC2 Default User <ec2-user@ip-172-31-3-10.us-east-2.compute.internal>
2021-10-09 09:40:23 +0000

    find movie with same director

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -37,6 +37,12 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def search_directors
+    @movie = Movie.find(params[:id])
+    @movies = Movie.find_similar_movies(@movie.director)
+    render 'similar_movies'
+  end
 
   private
   # Making "internal" methods private is not required, but is a common practice.
